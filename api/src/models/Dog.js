20e1c467d380095d67ebff32c51b0e6c98a3b4ec
LeslieQuet <quetglaslesllie@gmail.com>
2023-04-10 20:04:49 -0300

    DB Models Dogs y Temperment

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -3,10 +3,31 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
+  sequelize.define('Dog', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    image: {
+      type: DataTypes.BLOB,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    years: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
   });
 };
