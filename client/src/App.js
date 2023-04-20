7926e08c61e5a6ea2535407a0031a9edfbeab4a7
LeslieQuet <quetglaslesllie@gmail.com>
2023-04-20 16:50:59 -0300

    Functional Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import Home from './Components/Home'
+import BreedFinder from './Components/BreedFinder';
+import ImportingTemps from './Components/ImportingTemps';
+import About from './Components/About';
+import {Route, Switch, Link} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
+      <Link to="/home">
+        <button>Hora de ver perritos</button>
+      </Link>
+      <div className='AppContainer'>
+        <Switch>
+          {/* <Route path="/" element={<Landing/>} /> */}
+          <Route path="/dogs" element={<Home/>} />
+          <Route path="/dogs/:id" element= {<BreedFinder/>}/>
+          <Route path="/temperments" element= {<ImportingTemps/>}/>
+          <Route path= "/about" element= {<About/>}/>
+        </Switch>
+      </div> */
     </div>
   );
 }
