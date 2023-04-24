44ab62c3eb28670366df7a52d2a68d183e97c23b
LeslieQuet <quetglaslesllie@gmail.com>
2023-04-24 14:34:38 -0300

    Controller getDogsByBreed brings back an object

--- api/src/controllers/getDogs.js
+++ api/src/controllers/getDogs.js
@@ -8,9 +8,20 @@ const {Dog, Temperament} = require('../db');
 
 const dogsGetter = async (name) => {
     if(name){
-        const breedByName = await axios.get(`${URL_API_Q}${name}`)
-        if(Object.entries(breedByName.data).length === 0) throw Error ('No hay concidencias de raza')
-        return breedByName.data; 
+        const breedByQuery = await axios.get(`${URL_API_Q}${name}`)
+        if(Object.entries(breedByQuery.data).length === 0) throw Error ('No hay concidencias de raza')
+        const breedByName = breedByQuery.data.map(breed=>{
+            return {
+                id: breed.id,
+                image: `https://cdn2.thedogapi.com/images/${breed.reference_image_id}.jpg`,
+                name: breed.name,
+                height: breed.height.metric,
+                weight: breed.weight.metric,
+                life_span: breed.life_span,
+                temperament: breed.temperament,
+            }
+        })
+        return breedByName; 
     }
     else {
         const allDogsApi = await axios.get(`${URL_API}?api_key=${KEY}`)
@@ -23,7 +34,19 @@ const dogsGetter = async (name) => {
                 },
             }
         })
-    const allDogs = allDogsDB.concat(allDogsApi.data);
+
+    const apiDogs = allDogsApi.data.map(dog=>{
+        return {
+            id: dog.id,
+            image: `https://cdn2.thedogapi.com/images/${dog.reference_image_id}.jpg`,
+            name: dog.name,
+            height: dog.height.metric,
+            weight: dog.weight.metric,
+            life_span: dog.life_span,
+            temperament: dog.temperament,
+        }
+    })    
+    const allDogs = allDogsDB.concat(apiDogs);
     return allDogs;
     }
 }
